d767443720a1233711a119fdc6ed6f2171ae3fdf
anandkumarprajap <anandkumar7738@gmail.com>
2024-09-04 01:06:29 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Anand kumar",
+    job: "Software Engineer",
+    image: "https://i.ibb.co/x6Q26Py/IMG-20240309-000255-095.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Nikhil Kumar",
+    job: "Graphic Designer",
+    image: "https://i.ibb.co/MkhMpNP/Beauty-Plus-20181123113024406-save.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Adarsh Kumar",
+    job: "Marketing Manager",
+    image: "https://i.ibb.co/XWqjhnk/Screenshot-2024-09-03-18-36-00-58-1c337646f29875672b5a61192b9010f9.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Rishi Raj",
+    job: "Content Strategist",
+    image: "https://i.ibb.co/mTbF6Z8/IMG20240124164004.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Sachin Kumar",
+    job: "Data Analyst",
+    image: "https://i.ibb.co/8jwVwFb/IMG20240708055937.jpg",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
